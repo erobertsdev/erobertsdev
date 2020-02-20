50c14f218af92970d4e65f2854b03996150704e1
Elias Roberts <55095594+erobertsdev@users.noreply.github.com>
2020-02-20 07:57:55 -0700

    removed unused code

--- home/scripts/script.js
+++ home/scripts/script.js
@@ -1,25 +1,6 @@
 const menuButton = document.querySelectorAll('.menu-item');
 const menuCheckbox = document.querySelector('.menu-checkbox');
-const js30 = document.querySelector('.js-projects');
-const js30btn = document.getElementById('js30-btn');
-const projectCards = document.querySelectorAll('.js-project');
-let grown = false;
 
 for (let button of menuButton) {
 	button.addEventListener('click', () => (menuCheckbox.checked = false));
 }
-
-js30btn.addEventListener('click', () => {
-	if (!grown) {
-		js30btn.textContent = 'Close';
-		grown = true;
-	} else {
-		js30btn.textContent = 'Insert Coin';
-		grown = false;
-	}
-	js30.classList.toggle('grow');
-	for (let project of projectCards) {
-		project.classList.toggle('hide');
-		project.classList.toggle('project-fade-in');
-	}
-});
