07ac771840d0d90c24ab7db7c88ca891b3d4d183
Elias Roberts <elias@eroberts.dev>
2020-09-07 08:32:33 -0600

    added freelance status

--- home/scripts/script.js
+++ home/scripts/script.js
@@ -11,27 +11,27 @@ for (let button of menuButton) {
 	button.addEventListener('click', () => (menuCheckbox.checked = false));
 }
 
-designCard.addEventListener('click', () => {
-	switch (projectRequest.designCount) {
-		case 0:
-			designCard.innerHTML = `<h3 class="card-transition">STEP 1</h3>`;
-			projectRequest.designCount++;
-			break;
-		case 1:
-			designCard.innerHTML = `<h3 class="card-transition">STEP 2</h3>`;
-			projectRequest.designCount++;
-			break;
-		case 2:
-			designCard.innerHTML = `<h3 class="card-transition">STEP 3</h3>`;
-			projectRequest.designCount++;
-			break;
-		case 3:
-			designCard.innerHTML = `<h3 class="card-transition">STEP 4</h3>`;
-			projectRequest.designCount++;
-			break;
-		case 4:
-			designCard.innerHTML = `<h3 class="card-transition">STEP 5</h3>`;
-			projectRequest.designCount++;
-			break;
-	}
-});
+// designCard.addEventListener('click', () => {
+// 	switch (projectRequest.designCount) {
+// 		case 0:
+// 			designCard.innerHTML = `<h3 class="card-transition">STEP 1</h3>`;
+// 			projectRequest.designCount++;
+// 			break;
+// 		case 1:
+// 			designCard.innerHTML = `<h3 class="card-transition">STEP 2</h3>`;
+// 			projectRequest.designCount++;
+// 			break;
+// 		case 2:
+// 			designCard.innerHTML = `<h3 class="card-transition">STEP 3</h3>`;
+// 			projectRequest.designCount++;
+// 			break;
+// 		case 3:
+// 			designCard.innerHTML = `<h3 class="card-transition">STEP 4</h3>`;
+// 			projectRequest.designCount++;
+// 			break;
+// 		case 4:
+// 			designCard.innerHTML = `<h3 class="card-transition">STEP 5</h3>`;
+// 			projectRequest.designCount++;
+// 			break;
+// 	}
+// });
