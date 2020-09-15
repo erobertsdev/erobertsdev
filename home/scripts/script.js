7326a846605f9a4a0a0757e1b487a098f93b5c2b
Elias Roberts <elias@eroberts.dev>
2020-09-14 18:28:30 -0600

    added hike-share, some styling updates

--- home/scripts/script.js
+++ home/scripts/script.js
@@ -10,28 +10,3 @@ const projectRequest = {
 for (let button of menuButton) {
 	button.addEventListener('click', () => (menuCheckbox.checked = false));
 }
-
-// designCard.addEventListener('click', () => {
-// 	switch (projectRequest.designCount) {
-// 		case 0:
-// 			designCard.innerHTML = `<h3 class="card-transition">STEP 1</h3>`;
-// 			projectRequest.designCount++;
-// 			break;
-// 		case 1:
-// 			designCard.innerHTML = `<h3 class="card-transition">STEP 2</h3>`;
-// 			projectRequest.designCount++;
-// 			break;
-// 		case 2:
-// 			designCard.innerHTML = `<h3 class="card-transition">STEP 3</h3>`;
-// 			projectRequest.designCount++;
-// 			break;
-// 		case 3:
-// 			designCard.innerHTML = `<h3 class="card-transition">STEP 4</h3>`;
-// 			projectRequest.designCount++;
-// 			break;
-// 		case 4:
-// 			designCard.innerHTML = `<h3 class="card-transition">STEP 5</h3>`;
-// 			projectRequest.designCount++;
-// 			break;
-// 	}
-// });
